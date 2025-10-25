34c2e5f5d794d533968727bbcc3bc6b2866fef25
milko <milko.roine@aalto.fi>
2025-10-25 13:13:43 +0300

    New client with updated dependencies

--- RoboHack/client/robot_client.py
+++ RoboHack/client/robot_client.py
@@ -4,19 +4,24 @@ import threading
 import time
 import numpy as np
 import cv2
-import cv2
-from lerobot.hardware.so101 import SO101Env
+from lerobot.async_inference.configs import RobotClientConfig
 from lerobot.async_inference.robot_client import RobotClient
+import gymnasium as gym
+import mediapipe as mp
+from lerobot.cameras.opencv import OpenCVCameraConfig
+from lerobot.robots.so101_follower import SO101FollowerConfig
 
+mp_drawing = mp.solutions.drawing_utils
 
 SO101_PORT = "COM6"  #USB PORT
 SERVER_IP = "65.108.32.147"
 SERVER_PORT = 8000
+CAMERA_INDEX = 0
 
 
 class FaceTrackerThread(threading.Thread):
 
-    def __init__(self, env, stop_event, camera_index=0):
+    def __init__(self, env, stop_event):
         super().__init__()
         self.env = env
         self.stop_event = stop_event
@@ -47,6 +52,7 @@ class FaceTrackerThread(threading.Thread):
                     image_rgb.flags.setflags(write=0)
                     results = face_detection.process(image_rgb)
 
+
                     action = np.zeros_like(self.env.action_space.sample())
 
                     if results.detections:
@@ -71,7 +77,7 @@ class FaceTrackerThread(threading.Thread):
 
                     cv2.imshow(window_name, image_display)
 
-                    if cv2.waitKey(1) & 0xFF == 27:  # 27 is the ESC key
+                    if cv2.waitKey(1) & 0xFF == 27:
                         self.stop_event.set()
                         break
 
@@ -80,7 +86,7 @@ class FaceTrackerThread(threading.Thread):
                     if terminated or truncated:
                         obs, info = self.env.reset()
 
-                    time.sleep(0.02)  # ~50 Hz loop
+                    time.sleep(0.02)
 
         finally:
             print("Stopping idle process")
@@ -88,37 +94,67 @@ class FaceTrackerThread(threading.Thread):
 
 
 def main():
-    print("Connecting to robot... ")
-
+    print("Connecting to idle mode env.")
     try:
-        env = SO101Env(port=SO101_PORT, camera_index=CAMERA_INDEX)
+
+        idle_env = gym.make(
+            "LeRobot-SO101-Follower-v0",
+            port=SO101_PORT,
+            camera_index=CAMERA_INDEX
+        )
     except Exception as e:
         print(f"Failed to connect on {SO101_PORT} with camera {CAMERA_INDEX}")
         print(f"Error: {e}")
         return
 
     stop_event = threading.Event()
-    tracker_thread = FaceTrackerThread(env=env, stop_event=stop_event, camera_index=CAMERA_INDEX)
+    tracker_thread = FaceTrackerThread(env=idle_env, stop_event=stop_event)
     tracker_thread.start()
 
     print("\n Idle mode... ")
     instruction = input("Enter task: ")
 
-    print("Command received. Stopping idle mode... ")
+    print("Command received. Stopping idle thread...")
     stop_event.set()
     tracker_thread.join()
-    print("Idle mode stopped.")
+
+    idle_env.close()
+    print("Idle thread closed.")
 
     if instruction.lower() in ('q', 'quit'):
-        env.close()
         print("Connection closed")
         return
 
+    print("Connecting to OpenCV")
 
-    print("Initializing client for robot...")
-    client = RobotClient(env=env, host=SERVER_IP, port=SERVER_PORT)
+    camera_cfg = {
+        "primary": OpenCVCameraConfig(
+            index_or_path=CAMERA_INDEX,
+            width=640,
+            height=480,
+            fps=30
+        ),
+    }
 
-    print(f"Connecting to policy server at {SERVER_IP}:{SERVER_PORT}...")
+    robot_cfg = SO101FollowerConfig(
+        port=SO101_PORT,
+        id="follower_so101",
+        cameras=camera_cfg
+    )
+
+    client_cfg = RobotClientConfig(
+        robot=robot_cfg,
+        server_address=f"{SERVER_IP}:{SERVER_PORT}",
+        policy_device="cuda",
+        policy_type="hf_policy",
+        pretrained_name_or_path="openvla/openvla-7b",
+        chunk_size_threshold=0.7,
+        actions_per_chunk=50,
+    )
+
+    client = RobotClient(client_cfg)
+
+    print(f"Connecting to server at {client_cfg.server_address}...")
 
     if client.start():
         print("Connected to server!")
@@ -127,6 +163,7 @@ def main():
 
         try:
             print(f"Executing: '{instruction}'. Press Ctrl+C to stop.")
+            # 5. Run control loop with the task
             client.control_loop(instruction)
 
         except KeyboardInterrupt:
@@ -138,8 +175,7 @@ def main():
     else:
         print("Failed to connect to the policy server.")
 
-    env.close()
-    print("Robot connection closed.")
+    print("Task finished.")
 
 
 if __name__ == "__main__":
